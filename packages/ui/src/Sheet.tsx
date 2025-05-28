f2dd95e051a15931084df35d55b7d86935344151
ohad <ohad111@gmail.com>
2025-05-29 00:23:07 +0200

    change chord detecting regex to better match chords

--- packages/ui/src/Sheet.tsx
+++ packages/ui/src/Sheet.tsx
@@ -4,6 +4,14 @@ import styled from "styled-components";
 import Chord from "./Chord";
 import React from "react";
 import useKlankStore from "web/state/store";
+import {
+  delimiterMatcher,
+  testChords,
+  testHeader,
+  testSpaces,
+  testTokenContext,
+  transposeChord
+} from "@repo/sdk/chords";
 
 const SheetWrapper = styled.div<{$mode: string}>`
     overflow-y: ${props => props.$mode === 'Edit' ? 'none' : 'auto'};
@@ -24,72 +32,15 @@ const Lyric = styled.div`
     margin-bottom: 16px;
 `
 
-const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
-
-const delimiterMatcher = /(?<whitespace>\s+|\||\(|\))/
-
-const chordMatcher = "((?<note>[A-G])(?<accidentals>(?:bb|b|♭♭|♭)|(?:##|#))?)(?<chords>([Mm]|maj|min|sus|dim|add)?(b|bb|♭|♭♭)?(#|##)?([1-9]|1[0-9]|2[0-3])?([Mm]|maj|min|sus|dim|add)?([1-9]|1[0-9]|2[0-3])?)?(?:\\/(?<bass>(?<bassNote>[A-G])(?<bassAccidentals>(?:b|bb|♭|♭♭)|(?:#|##))?))?"
-
-const testHeader = (string: string) => /\[[a-zA-Z0-9\s]+/.test(string)
-const testChords = (string: string) => {
-  const match = (new RegExp("^" + chordMatcher + "$")).exec(string)
-
-  if (match === null || match.length === 0)
-    return false
-  if (match[0] === string)
-    return true
-}
-const testSpaces = (string: string) => /^\s*$/.test(string)
-
-const getNoteOffset = (string: string | undefined) => {
-  if (string === undefined) {
-    return 0
-  }
-  const regex = /^(?<flats>bb|b|♭♭|♭)|(?<sharps>##|#)$/
-  const accidentals = regex.exec(string)
-
-  if (accidentals === null) {
-    return 0
-  } else if (["b", "bb", "♭", "♭♭"].includes(accidentals[0])) {
-    return string.length * -1
-  }
-  return string.length
-}
-
-const normalizeNoteIndex = (noteNumber: number): number => ((noteNumber % notes.length) + notes.length) % notes.length
-
-const matchChords = (string: string) => (new RegExp(chordMatcher).exec(string))
-
-const transposeChord = (chord: string, transpose: number): string => {
-  const currentNote = matchChords(chord)?.groups
-  const matchedChords = currentNote?.chords
-
-  if (transpose !== 0) {
-    const noteIndex = notes.findIndex(sequenceNote => sequenceNote === currentNote?.note)
-    const bassNoteIndex = notes.findIndex(sequenceNote => sequenceNote === currentNote?.bassNote)
-
-    const transposedNoteIndex = normalizeNoteIndex(noteIndex + getNoteOffset(currentNote?.accidentals ?? '') + transpose)
-    const transposedBassNoteIndex = currentNote?.bassNote !== undefined ? normalizeNoteIndex(bassNoteIndex + getNoteOffset(currentNote.bassAccidentals) + transpose) : null
-
-    const bassChord = transposedBassNoteIndex !== null
-      ? `/${notes[transposedBassNoteIndex]}`
-      : '';
-
-    return `${notes[transposedNoteIndex]}${bassChord}${matchedChords ?? ''}`
-  }
-  return chord
-}
-
 const lineMatcher = (line: string, index: number, transpose: number): React.ReactNode => {
   const tokens = line.split(delimiterMatcher).filter(token => token !== '')
-  
-  // Check if the line contains any valid chords
+
+  const isComplexToken = (tokens[0] === 'A' || tokens[0] === 'Am') ? testTokenContext(tokens) : false
   const hasValidChords = tokens.some(token => testChords(token.replace('|', '')))
 
-  // If any token is a valid chord, or it's a single complex chord
-  if (hasValidChords || (tokens.length === 1 && tokens[0] !== undefined && testChords(tokens[0].replace('|', '')))) {
+  if (hasValidChords && !isComplexToken) {
     const processedChords = line.split(delimiterMatcher).map((currentValue, i) => {
-      if (testChords(currentValue.replace('|', ''))) {
+      if (testChords(currentValue)) {
         return <Chord key={currentValue + index + i}>{transposeChord(currentValue, transpose)}</Chord>
       }
       return <React.Fragment key={currentValue + index + i}>{currentValue}</React.Fragment>
